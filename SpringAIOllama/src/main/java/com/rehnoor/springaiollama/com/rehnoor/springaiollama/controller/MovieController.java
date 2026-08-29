07f53470eb248c1b6f42dbe74bddb18e2b0d50ee
Rehnoor Aulakh <aulakhrehnoor@gmail.com>
2026-08-29 08:20:51 +0530

    Movie finding by Actor Name with List<String> format by using ListOutputConverter to specify format

---
+++ SpringAIOllama/src/main/java/com/rehnoor/springaiollama/com/rehnoor/springaiollama/controller/MovieController.java
@@ -0,0 +1,56 @@
+package com.rehnoor.springaiollama.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.converter.ListOutputConverter;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class MovieController {
+
+    private ChatClient chatClient;
+
+    ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
+
+    public MovieController(ChatClient.Builder builder) {
+        this.chatClient = builder
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .build();
+    }
+
+    @GetMapping("movies")
+    public List<String> getMovies(@RequestParam String name) {
+        String message = """
+                    List top 5 movies of {name}
+                    {format}
+                """;
+        ListOutputConverter outputConverter = new ListOutputConverter(new DefaultConversionService());
+        PromptTemplate template = PromptTemplate.builder().template(message).variables(Map.of("name", name, "format", outputConverter.getFormat())).build();
+        Prompt prompt = template.create();
+        List<String> movies = outputConverter.convert(
+                chatClient
+                        .prompt(prompt)
+                        .advisors(a -> a.param(
+                                ChatMemory.CONVERSATION_ID,
+                                "user1"
+                        ))
+                        .call()
+                        .content()
+        );
+        return movies;
+    }
+}
