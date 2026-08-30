874d78c42e03bd9f19d0c2e05fd89cbdb7317b48
Rehnoor Aulakh <aulakhrehnoor@gmail.com>
2026-08-30 07:22:16 +0530

    Image generation completed

--- SpringAIOllama/src/main/java/com/rehnoor/springaiollama/com/rehnoor/springaiollama/controller/ImageGenController.java
+++ SpringAIOllama/src/main/java/com/rehnoor/springaiollama/com/rehnoor/springaiollama/controller/ImageGenController.java
@@ -4,9 +4,10 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
-import org.springframework.ai.image.ImageModel;
-import org.springframework.ai.image.ImagePrompt;
-import org.springframework.ai.image.ImageResponse;
+import org.springframework.ai.image.*;
+import org.springframework.ai.stabilityai.StabilityAiImageModel;
+import org.springframework.ai.stabilityai.StyleEnum;
+import org.springframework.ai.stabilityai.api.StabilityAiImageOptions;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,16 +19,21 @@ import java.util.Base64;
 public class ImageGenController {
     private ChatClient chatClient;
 
-    private ImageModel imageModel;
+    private StabilityAiImageModel imageModel;
 
-    public ImageGenController(ChatClient.Builder builder, ImageModel imageModel) {
+    public ImageGenController(ChatClient.Builder builder, StabilityAiImageModel imageModel) {
         this.chatClient = builder.build();
         this.imageModel = imageModel;
     }
 
     @GetMapping(value = "/image/{query}")
     public ResponseEntity<?> genImage(@PathVariable String query) {
-        ImagePrompt prompt = new ImagePrompt(query);
+        ImagePrompt prompt = new ImagePrompt(query, StabilityAiImageOptions.builder()
+                .steps(30)
+                .height(1024)
+                .width(1024)
+                .stylePreset("cinematic")
+                .build());
         ImageResponse response = imageModel.call(prompt);
         
         String b64Json = response.getResult().getOutput().getB64Json();
